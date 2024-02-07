f7fe3f74473c9bb71e7a555265da4fa3afaa4865
wipaweeeeee <wsirikolkarn@gmail.com>
2024-02-06 21:00:52 -0500

    fix fonts issue, add description

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -13,6 +13,7 @@ const Home = () => {
     const [submitted, setSubmitted] = useState(false);
     const [openModal, setOpenModal] = useState(false);
     const [mobile, setMobile] = useState(false);
+    const [openDesc, setOpenDesc] = useState(false);
 
     const base = new Airtable({apiKey: `${import.meta.env.VITE_AIRTABLE_API_KEY}`}).base(`${import.meta.env.VITE_AIRTABLE_BASE_ID}`);
     const table = base('signups');
@@ -108,8 +109,47 @@ const Home = () => {
         }
     }
 
+    const descVariant = {
+        open: {
+            opacity: 1,
+            zIndex: 2,
+            scale: 1,
+            x: '-50%',
+            y: '-50%',
+            transition: {
+                duration: 0.25,
+                easing: easeIn
+            }
+        }, 
+        close: {
+            opacity: 0,
+            zIndex: -1,
+            scale: 0,
+            x: '-50%',
+            y: '-50%',
+            transition: {
+                duration: 0.25,
+                easing: easeIn
+            }
+        }
+    }
+
     return (
         <div>
+            <div className={styles.descCTA} onClick={() => setOpenDesc(!openDesc)}/>
+            <motion.div 
+                className={styles.desc}
+                initial={false}
+                variants={descVariant}
+                animate={openDesc ? 'open' : 'close'}
+            >
+                <p>
+                    SHIFT brings together the varied work of five emerging artists and technologists originally from Thailand to demonstrate that neither art nor technology can be understood through just one perspective and audience. Instead of technology being the sole property of nerds, tech bros, venture capitalists, could it instead function as a softer, artistic canvas to express something painful, moving, or strange? Instead of art being a conversation solely between painters, hipsters, gallerists, and collectors, what can those fluent in the language of technology bring to the conversation?
+                </p>
+                <p>
+                    The work in this show probes and plays with these dynamics through new ideas, deviant strategies, and absurd forms of technology, code, and pixels — proving that technology can be a fertile canvas used to challenge and critique our interaction with daily life, question the notion of beauty, challenge the concept of time, or have no meaningful 'functionality' at all.
+                </p>
+            </motion.div>
             <motion.div 
                 className={styles.modalContainer}
                 variants={modalVariant}
