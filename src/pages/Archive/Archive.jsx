2c524d48d64c97f10740b48e290145b7787e1fce
wipaweeeeee <wsirikolkarn@gmail.com>
2025-03-27 18:00:40 -0400

    mid way thru building archive page

--- src/pages/Archive/Archive.jsx
+++ src/pages/Archive/Archive.jsx
@@ -1,19 +1,51 @@
-import { Canvas } from '@react-three/fiber';
-import { Suspense, useEffect, useState } from 'react';
-import Bg from '@/components/Bg';
+import { useEffect, useState } from 'react';
 import styles from './styles.module.scss';
 import useLangState from '@/atoms/language_atom';
 // import { data } from './content';
 import classNames from 'classnames';
+import Marquee from "react-fast-marquee";
 
 import Airtable from 'airtable';
+
+const ArchiveImage = () => {
+
+    const [hover, setHover] = useState(false)
+
+    return (
+        <div
+            className={classNames(styles.test, {[styles.hover] : hover})} 
+            onMouseOver={() => setHover(true)}
+            onMouseLeave={() => setHover(false)}
+        >
+            <img src="test.jpg" />
+            <svg>
+                <filter id="filter1" colorInterpolationFilters="sRGB" x="0" y="0" width="100%" height="100%">
+                    <feFlood floodColor="#000000" floodOpacity="0.20" x="0%" y="0%" result="flood"/>
+                    <feBlend mode="normal" x="0%" y="0%" in="SourceGraphic" in2="flood" result="blend1"/>
+                    <feImage className="ditherImage" xlinkHref="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAgAAAAICAYAAADED76LAAAA5ElEQVQYlQXBgQbCUABA0fdrk0ySSZJJkiRJMjOTTGZmkiRJZiYzyczMzGQmfdrtHPH7/TgcDuR5zna7pWka9vs9aZqyXq8R0+mU5/OJoihcLhfG4zFBENDtdjmdToj3+81yueTz+WCaJnEcM5/PKcsSXdcRsizjeR6j0YjH40Gr1cJxHAaDAbfbDVHXNbvdjiRJWK1WfL9fLMsiyzI2mw1CVVV836fT6XA8HplMJoRhSK/X43w+I6IoYjabURQFmqbxer1YLBZUVYVhGAhJkrBtm36/z/V6pd1u47ouw+GQ+/3OH4/Fn8FvF/NxAAAAAElFTkSuQmCC" x="0" y="0" width="4" height="4" crossOrigin="anonymous" result="image1"/>
+                    <feTile x="0" y="0"  in="image1" result="tile"/>
+                    <feBlend mode="overlay" x="0%" y="0%"  in="blend1" in2="tile" result="blend2"/>
+                    <feColorMatrix type="saturate" values="0.1"/>
+                    <feComponentTransfer>
+                        <feFuncR type="discrete" tableValues="0 0"/>
+                        <feFuncG type="discrete" tableValues="0 1"/>
+                        <feFuncB type="discrete" tableValues="1 0"/>
+                    </feComponentTransfer>
+                </filter>
+            </svg>
+    </div>
+    )
+}
 
 const Archive = () => {
 
     const [mobile, setMobile] = useState(false);
     const [{lang}] = useLangState();
     // let content = data[lang];
-    
+
+    const [hover, setHover] = useState(false);
+    const [showPhoto, setShowPhoto] = useState(false);
+
     useEffect(() => {
 
         const handleResize = () => {
@@ -33,12 +65,49 @@ const Archive = () => {
     },[])
 
     return (
-        <div className={styles.canvas}>
-            <Canvas camera={{ position: [0, 0, 8], fov: 50 }}>
-                <Suspense fallback={null}>                            
-                    <Bg />
-                </Suspense>
-             </Canvas>
+        <div className={styles.archivePage}>
+            <div className={classNames(styles.eventCount, {[styles.hide] : showPhoto})}>
+                <img src="cache.svg" className={styles.cache}/>
+                <div className={styles.index}>
+                    001
+                </div>
+            </div>
+            <div className={classNames(styles.imagesContainer, {[styles.hide] : showPhoto})}>
+                <ArchiveImage />
+                <ArchiveImage />
+                <ArchiveImage />
+                <ArchiveImage />
+            </div>
+            <div className={classNames(styles.eventName, {[styles.show] : showPhoto})}>
+                <div className={styles.title}>Shift</div>
+                <div className={styles.moreButton} onClick={() => setShowPhoto(!showPhoto)}>
+                    <span>see more</span>
+                    <span>&rarr;</span>
+                </div>
+                <div className={styles.eventDetail}>
+                    <div>April 4–15, 2024</div>
+                    <div>1559 SPACE</div>
+                    <div>Bangkok, Thailand</div>
+                </div>
+            </div>
+            <div className={classNames(styles.eventPhoto, {[styles.show] : showPhoto})}>
+                <img src="test.jpg" />
+                <img src="test.jpg" />
+                <img src="test.jpg" />
+                <img src="test.jpg" />
+                <img src="test.jpg" />
+                <img src="test.jpg" />
+                <img src="test.jpg" />
+                <img src="test.jpg" />
+            </div>
+            {/* <div className={styles.marquee}> 
+                <Marquee autoFill speed={5} >
+                    <ArchiveImage />
+                    <ArchiveImage />
+                    <ArchiveImage />
+                    <ArchiveImage />
+                </Marquee>
+            </div>  */}
         </div>
     )
 }
